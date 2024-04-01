cdad94383420f1d710d7a75871a4378fdfeb9f32
Juan Manuel Gonzalez Calleros <jumagoca78@users.noreply.github.com>
2024-03-31 19:27:55 -0600

    Corregido el error de no encontrar al más grande por una inicialización no adecuada

--- 01 Encontrar el más grande/ElMasGrande project/src/TestElMasGrande.java
+++ 01 Encontrar el más grande/ElMasGrande project/src/TestElMasGrande.java
@@ -1,15 +1,12 @@
-package lib.Test;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import lib.ElMasGrande; // Add this line
 
 public class TestElMasGrande{
     
     @Test
     public void testLargestAtBegginning() {
-        int[] arr = {5, 3, 2, 1, 4};
-        int result = ElMasGrande.largest(arr);
-        assertEquals(5, result);
+        int[] array = {5, 4, 3, 2, 1};
+        assertEquals(5, ElMasGrande.largest(array));
     }
     
 }
